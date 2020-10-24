50a14269d1d62dbe3db1449454a1e77dc5702f19
Gemma Black <gblackuk@googlemail.com>
2020-10-24 13:37:30 +0100

    laravel with docker

---
+++ docker/entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/sh -e
+# Alpine shell is `sh`
+# Provision the environment in here!
+
+echo "Starting app entrypoint..."
+
+case $1 in
+
+  permissions)
+    cd /var/www
+
+    echo "Ensuring laravel logs directory existence and permissions..."
+    mkdir -p /storage/logs
+    mkdir -p /storage/app/public
+    mkdir -p /storage/framework/cache
+    mkdir -p /storage/framework/sessions
+    mkdir -p /storage/framework/testing
+    mkdir -p /storage/framework/views
+    chown -R 1000:1000 /storage
+    chmod -R 0777 /storage
+  ;;
+
+  decache)
+    echo "Decaching"
+      php artisan cache:clear
+      php artisan config:clear
+      php artisan view:clear
+  ;;
+
+  db:refresh)
+    php artisan migrate:refresh --seed
+    php artisan ide-helper:models --nowrite # Has to be run with access to DB
+  ;;
+
+  db:migrate)
+    php artisan migrate
+  ;;
+
+  server)
+    ./$0 permissions
+    ./$0 decache
+
+    php-fpm
+  ;;
+
+  *)
+    echo "Running container commands..."
+    exec "$@"
+  ;;
+
+esac
+
+echo "Completed app entrypoint!"
+
+exit 0
